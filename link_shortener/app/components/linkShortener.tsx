e05c5e6af6b513730d388e9c94a8c61915384d15
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-07-29 10:11:03 -0300

    .

--- link_shortener/app/components/linkShortener.tsx
+++ link_shortener/app/components/linkShortener.tsx
@@ -1,9 +1,55 @@
 'use client'
+import { useState } from "react"
 
-const linkShortener = () => {
+const LinkShortener = () => {
+  const [linkUrl, setLinkUrl] = useState('')
+  const [newText, setNewText] = useState('')
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = async () => {
+    if (!linkUrl || !newText) return
+
+    const htmlContent = `<a href="${linkUrl}">${newText}</a>`
+    const plainText = linkUrl
+
+    try {
+      const item = new ClipboardItem({
+        "text/html": new Blob([htmlContent], {type: "text/html"}),
+        "text/plain": new Blob([plainText], {type: "text/plain"})
+      })
+
+      await navigator.clipboard.write([item])
+      setCopied(true)
+    } catch (err) {
+      try {
+        await navigator.clipboard.writeText(plainText)
+        setCopied(true)
+      } catch {
+        console.log("Não foi possível copiar: ", err)
+      }
+    }
+  }
+
   return (
-    <div>linkShortener</div>
+    <div className="bg-[#2a2828] flex flex-col items-center justify-center h-70 w-150 rounded-md gap-8">
+
+      <input type="text" placeholder="URL aqui:" value={linkUrl} onChange={(e) => setLinkUrl(e.target.value)}
+      className="bg-[#dcdada] w-80 p-1 rounded"/>
+
+      <input type="text" placeholder="Novo texto aqui" value={newText} onChange={(e) => setNewText(e.target.value)} className="bg-[#dcdada] w-80 p-1 rounded"/>
+
+      {linkUrl && newText && (
+        <p className="text-white">
+          Preview: <a href={linkUrl} target="_blank" rel="noopener noreferrer">{newText}</a>
+        </p>
+      )}
+
+      <button onClick={handleCopy}
+      className="bg-[#A6D3A0] w-80 p-1 rounded cursor-pointer">
+        {copied ? 'Copiado!' : 'Copiar link'}
+      </button>
+    </div>
   )
 }
 
-export default linkShortener
+export default LinkShortener
--- link_shortener/app/page.tsx
+++ link_shortener/app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
+import LinkShortener from "./components/linkShortener"
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className="w-screen h-screen bg-[#312F2F] gap-10 flex flex-col items-center justify-center">
+      <h1 className="text-white text-xl font-bold">Encurtador de URL</h1>
+      <LinkShortener/>
+    </div>
   )
 }
 
